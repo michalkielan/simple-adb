ddbe67d1d181f1acf132ceb09ae580e666e71ce6
Michal Kielan <kielanm@fb.com>
2019-05-08 16:58:08 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='simpleadb',
+   version='0.1',
+   description='Adb wrapper',
+   author='Micha Kielan',
+   author_email='michalkielan@protonmail.com',
+   packages=['simpleadb'],
+   install_requires=['subprocess'],
+)
