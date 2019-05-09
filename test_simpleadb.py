7d05be16b17cc04d59ce1ebd866263123058e0ff
Michal Kielan <michalkielan@protonmail.com>
2019-05-09 07:10:48 +0200

    try run test

--- test_simpleadb.py
+++ test_simpleadb.py
@@ -1,4 +1,10 @@
 import simpleadb
 
+def get_emulator_id():
+  return 'emulator-5554'
+
 class AdbServerTest(unittest.TestCase):
-  pass
+  def tst_adb_devices(self):
+    adb_server = AdbServer()
+    devices = adb_server.devices()
+    self.assertEqual(devices[0].get_id(), get_emulator_id())
