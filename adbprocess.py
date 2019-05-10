b1ea903e668106637f01f8b51d279c9024db0703
Michal Kielan <michalkielan@protonmail.com>
2019-05-10 10:11:36 -0700

    fix process

--- adbprocess.py
+++ adbprocess.py
@@ -1,11 +1,11 @@
 import subprocess
 
-def call(cmd):
-  return subprocess.call(['adb ', cmd])
+def call(args):
+  return subprocess.call(['adb ' + args], shell=True)
 
-def check_call(cmd):
-  return subprocess.check_call(['adb', cmd])
+def check_call(args):
+  return subprocess.check_call(['adb ' + args], shell=True)
 
-def check_output(cmd):
-  return subprocess.check_output('adb ' + cmd, shell=True)
+def check_output(args):
+  return subprocess.check_output('adb ' + args, shell=True)
 
