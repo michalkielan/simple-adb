7960c3c1be2932c5f3e7f491abe66798844a36f7
Michal Kielan <michalkielan@protonmail.com>
2019-05-10 07:37:24 +0200

    typo

--- test_simpleadb.py
+++ test_simpleadb.py
@@ -6,12 +6,12 @@ def get_emulator_id():
 
 class AdbServerTest(unittest.TestCase):
   def test_devices(self):
-    adb_server = AdbServer()
+    adb_server = simpleadb.AdbServer()
     devices = adb_server.devices()
-    self.assertTrue(get_emulator_id() in (devices[0].get_id())
+    self.assertTrue(get_emulator_id() in devices[0].get_id())
 
   def test_root(self):
-    adb_server = AdbServer()
+    adb_server = simpleadb.AdbServer()
     devices = adb_server.devices()
     for device in devices:
       device.root()      
