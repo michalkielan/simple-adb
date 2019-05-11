eba3e672a55e001eba49d08a3b653751241a41e5
Michal Kielan <michalkielan@protonmail.com>
2019-05-11 08:32:18 +0200

    add adb prefix

--- adbprocess.py
+++ adbprocess.py
@@ -1,11 +1,14 @@
 import subprocess
 
+def get_adb_prefix():
+  return 'adb '
+
 def call(args):
-  return subprocess.call(['adb ' + args], shell=True)
+  return subprocess.call([get_adb_prefix() + args], shell=True)
 
 def check_call(args):
-  return subprocess.check_call(['adb ' + args], shell=True)
+  return subprocess.check_call([get_adb_prefix() + args], shell=True)
 
 def check_output(args):
-  return subprocess.check_output('adb ' + args, shell=True)
+  return subprocess.check_output(get_adb_prefix() + args, shell=True)
 
