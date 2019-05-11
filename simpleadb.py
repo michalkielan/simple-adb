ab280aa876fbbeffae143d53a3f69de698b97c8e
Michal Kielan <michalkielan@protonmail.com>
2019-05-11 08:38:28 +0200

    fix classes

--- simpleadb.py
+++ simpleadb.py
@@ -4,7 +4,7 @@ import adbprefixes
 def get_encoding_format():
   return 'utf-8'
 
-class AdbDevice(object):
+class AdbDevice():
   def __init__(self, device_id):
     self.__id = device_id
 
@@ -138,7 +138,7 @@ class AdbDevice(object):
     return self.__check_call(cmd)
 
 
-class AdbServer(object):
+class AdbServer():
   def __init__(self):
     pass
 
