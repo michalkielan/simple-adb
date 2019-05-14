6169636620fdc59a5dee2c8fddfd5e6adaa98cbe
Michal Kielan <michalkielan@protonmail.com>
2019-05-14 05:04:45 +0200

    restructure modules

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
    name='simpleadb',
-   version='0.1',
+   version='0.0.1',
    description='Adb wrapper',
    author='Micha Kielan',
    author_email='michalkielan@protonmail.com',
    packages=['simpleadb'],
-   install_requires=['subprocess'],
+   package_dir={'simpleadb':'simpleadb'},
+   test_suite="tests"
 )
