2f99c4f56d98c0a57e2ec0d6ab3f4507923eef92
Michal Kielan <michalkielan@protonmail.com>
2019-05-15 00:26:48 -0700

    More doc string, remove doc-string ignore in pylint (#28)

--- adbprocess.py
+++ adbprocess.py
@@ -1,7 +1,15 @@
+""" Interface for adb process"""
 import subprocess
 import adbprefixes
 
 def call(args):
+  """ Call process
+
+    Args:
+      Arguments
+    Returns:
+      0 if success, otherwise error code
+  """
   cmd = ' '.join([
       adbprefixes.get_adb_prefix(),
       args,
@@ -9,6 +17,15 @@ def call(args):
   return subprocess.call(cmd, shell=True)
 
 def check_call(args):
+  """ Call process
+
+    Args:
+      Arguments
+    Returns:
+      0 if success
+    Raises:
+      CalledProcessError: when failed
+  """
   cmd = ' '.join([
       adbprefixes.get_adb_prefix(),
       args,
@@ -16,6 +33,15 @@ def check_call(args):
   return subprocess.check_call(cmd, shell=True)
 
 def check_output(args):
+  """ Call process
+
+    Args:
+      Arguments
+    Returns:
+      0 if success
+    Raises:
+      CalledProcessError: when failed
+  """
   cmd = ' '.join([
       adbprefixes.get_adb_prefix(),
       args,
