a3528184c4e1788df66c1961be96ef90d3ac4737
Michal Kielan <michalkielan@protonmail.com>
2019-05-18 08:34:28 +0200

    split tests to separate files

---
+++ tests/test_adb_server.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import unittest
+import simpleadb
+
+def get_test_device_id():
+  return 'emulator-5554'
+
+TEST_DEVICE_ID = get_test_device_id()
+
+class AdbServerTest(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def test_devices(self):
+    adb = simpleadb.AdbServer()
+    devices = adb.devices()
+    if not devices:
+      self.fail('No adb devices found')
+    emulator = devices[0]
+    self.assertTrue(TEST_DEVICE_ID in emulator.get_id())
+
+
