09dba25fa94c7d73fef5b1e40ade0fb6cbf24ca6
Michał Kielan <kielanm@fb.com>
2019-05-20 16:37:08 -0700

    fix: ,

--- simpleadb/adbprocess.py
+++ simpleadb/adbprocess.py
@@ -15,7 +15,7 @@ class AdbProcess(object):
         0 if success, otherwise error code
     """
     cmd = ' '.join([
-        self.__adb_path
+        self.__adb_path,
         args,
     ])
     return subprocess.call(cmd, shell=True)
@@ -31,7 +31,7 @@ class AdbProcess(object):
         CalledProcessError: when failed
     """
     cmd = ' '.join([
-        self.__adb_path
+        self.__adb_path,
         args,
     ])
     return subprocess.check_call(cmd, shell=True)
@@ -47,7 +47,7 @@ class AdbProcess(object):
         CalledProcessError: when failed
     """
     cmd = ' '.join([
-        self.__adb_path
+        self.__adb_path,
         args,
     ])
     return subprocess.check_output(cmd, shell=True)
