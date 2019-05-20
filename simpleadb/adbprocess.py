37b5941b3f555352cb9a41bac6d594a810100f02
Michał Kielan <kielanm@fb.com>
2019-05-20 15:39:39 -0700

    create separate class for adbprocess

--- simpleadb/adbprocess.py
+++ simpleadb/adbprocess.py
@@ -2,48 +2,52 @@
 import subprocess
 from . import adbcmds
 
-def call(args):
-  """ Call process
+class AdbProcess(object):
+    def __init__():
+      self.__adb_path = adbcmds.ADB
 
-    Args:
-      Arguments
-    Returns:
-      0 if success, otherwise error code
-  """
-  cmd = ' '.join([
-      adbcmds.ADB,
-      args,
-  ])
-  return subprocess.call(cmd, shell=True)
+  def call(self, args):
+    """ Call process
 
-def check_call(args):
-  """ Call process
+      Args:
+        Arguments
+      Returns:
+        0 if success, otherwise error code
+    """
+    cmd = ' '.join([
+        self.__adb_path
+        args,
+    ])
+    return subprocess.call(cmd, shell=True)
 
-    Args:
-      Arguments
-    Returns:
-      0 if success
-    Raises:
-      CalledProcessError: when failed
-  """
-  cmd = ' '.join([
-      adbcmds.ADB,
-      args,
-  ])
-  return subprocess.check_call(cmd, shell=True)
+  def check_call(self, args):
+    """ Call process
 
-def check_output(args):
-  """ Call process
+      Args:
+        Arguments
+      Returns:
+        0 if success
+      Raises:
+        CalledProcessError: when failed
+    """
+    cmd = ' '.join([
+        self.__adb_path
+        args,
+    ])
+    return subprocess.check_call(cmd, shell=True)
 
-    Args:
-      Arguments
-    Returns:
-      0 if success
-    Raises:
-      CalledProcessError: when failed
-  """
-  cmd = ' '.join([
-      adbcmds.ADB,
-      args,
-  ])
-  return subprocess.check_output(cmd, shell=True)
+  def check_output(self, args, adb):
+    """ Call process
+
+      Args:
+        Arguments
+      Returns:
+        0 if success
+      Raises:
+        CalledProcessError: when failed
+    """
+    cmd = ' '.join([
+        self.__adb_path
+        args,
+    ])
+    return subprocess.check_output(cmd, shell=True)
