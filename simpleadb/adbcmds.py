98ca9200f504240f743197c656c640300d71c108
Michał Kielan <kielanm@fb.com>
2020-11-03 23:16:37 -0800

    lint pep8 style

--- simpleadb/adbcmds.py
+++ simpleadb/adbcmds.py
@@ -39,12 +39,13 @@ GET_STATE = 'get-state'
 VERSION = 'version'
 LOGCAT = 'logcat'
 
-def get_set_device(device_id):
-  """ Set specific device to adb command
 
-    Args:
-      Device id
-    Returns:
-      String to set device id in adb command
-  """
-  return '-s ' + str(device_id)
+def get_set_device(device_id):
+    """ Set specific device to adb command
+
+      Args:
+        Device id
+      Returns:
+        String to set device id in adb command
+    """
+    return '-s ' + str(device_id)
--- simpleadb/adbprocess.py
+++ simpleadb/adbprocess.py
@@ -2,71 +2,73 @@
 import subprocess
 from . import adbcmds
 
+
 class AdbProcess(object):
-  """AdbProcess this class is used to call adb process
-
-    Args:
-      path (Optional[str]): adb path, default: 'adb'
-    Returns:
-      0 if success, otherwise error code
-  """
-  def __init__(self, path=adbcmds.ADB):
-    self.__adb_path = path
-
-  def call(self, args, **kwargs):
-    """ Call process
+    """AdbProcess this class is used to call adb process
 
       Args:
-        args (str): Arguments
-        **kwargs: Arbitrary keyword arguments
-      Keyword Args:
-        timeout (int): Timeout in sec
+        path (Optional[str]): adb path, default: 'adb'
       Returns:
         0 if success, otherwise error code
     """
-    cmd = ' '.join([
-        self.__adb_path,
-        args,
-    ])
-    kwargs.setdefault('shell', True)
-    return subprocess.call(cmd, **kwargs)
 
-  def check_call(self, args, **kwargs):
-    """ Call process
+    def __init__(self, path=adbcmds.ADB):
+        self.__adb_path = path
 
-      Args:
-        args (str): Arguments
-        **kwargs: Arbitrary keyword arguments
-      Keyword Args:
-        timeout (int): Timeout in sec
-      Returns:
-        0 if success
-      Raises:
-        CalledProcessError: when failed
-    """
-    cmd = ' '.join([
-        self.__adb_path,
-        args,
-    ])
-    kwargs.setdefault('shell', True)
-    return subprocess.check_call(cmd, **kwargs)
+    def call(self, args, **kwargs):
+        """ Call process
 
-  def check_output(self, args, **kwargs):
-    """ Call process
+          Args:
+            args (str): Arguments
+            **kwargs: Arbitrary keyword arguments
+          Keyword Args:
+            timeout (int): Timeout in sec
+          Returns:
+            0 if success, otherwise error code
+        """
+        cmd = ' '.join([
+            self.__adb_path,
+            args,
+        ])
+        kwargs.setdefault('shell', True)
+        return subprocess.call(cmd, **kwargs)
 
-      Args:
-        args (str): Arguments
-        **kwargs: Arbitrary keyword arguments
-      Keyword Args:
-        timeout (int): Timeout in sec
-      Returns:
-        0 if success
-      Raises:
-        CalledProcessError: when failed
-    """
-    cmd = ' '.join([
-        self.__adb_path,
-        args,
-    ])
-    kwargs.setdefault('shell', True)
-    return subprocess.check_output(cmd, **kwargs)
+    def check_call(self, args, **kwargs):
+        """ Call process
+
+          Args:
+            args (str): Arguments
+            **kwargs: Arbitrary keyword arguments
+          Keyword Args:
+            timeout (int): Timeout in sec
+          Returns:
+            0 if success
+          Raises:
+            CalledProcessError: when failed
+        """
+        cmd = ' '.join([
+            self.__adb_path,
+            args,
+        ])
+        kwargs.setdefault('shell', True)
+        return subprocess.check_call(cmd, **kwargs)
+
+    def check_output(self, args, **kwargs):
+        """ Call process
+
+          Args:
+            args (str): Arguments
+            **kwargs: Arbitrary keyword arguments
+          Keyword Args:
+            timeout (int): Timeout in sec
+          Returns:
+            0 if success
+          Raises:
+            CalledProcessError: when failed
+        """
+        cmd = ' '.join([
+            self.__adb_path,
+            args,
+        ])
+        kwargs.setdefault('shell', True)
+        return subprocess.check_output(cmd, **kwargs)
