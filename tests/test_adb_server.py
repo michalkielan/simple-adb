55aaada88a8f9e6f047266af343373c57a1d8898
Michał Kielan <kielanm@fb.com>
2021-06-07 23:01:22 -0700

    Fix pylint and docstring

--- tests/test_adb_server.py
+++ tests/test_adb_server.py
@@ -1,10 +1,11 @@
-#!/bin/python3
+"""Unit tests for adb commands"""
 import os
 import unittest
 import simpleadb
 
 
 def get_test_device_id():
+    """Get test device serial number"""
     return os.environ['TEST_DEVICE_ID']
 
 
@@ -12,13 +13,15 @@ TEST_DEVICE_ID = get_test_device_id()
 
 
 class AdbServerTest(unittest.TestCase):
+    """Adb server unit tests"""
     def setUp(self):
-        pass
+        """Setup test"""
 
     def tearDown(self):
-        pass
+        """Teardown test"""
 
     def test_adb_devices(self):
+        """Check if adb server is running correctly"""
         adb = simpleadb.AdbServer()
         devices = adb.devices()
         if not devices:
