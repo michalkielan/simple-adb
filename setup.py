65f75c5cdff19d0d4a52b920d72fc43c2f3d2310
Michał Kielan <kielanm@fb.com>
2022-06-07 15:38:28 -0700

    Fix possible fd leak when module generation

--- setup.py
+++ setup.py
@@ -6,14 +6,18 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 
+"""Installation package"""
 import io
 from setuptools import setup, find_packages
+
+with io.open('README.rst', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
 
 setup(
     name='simpleadb',
     version='0.3.2dev',
     description='Python wrapper for adb protocol.',
-    long_description=io.open('README.rst', encoding='utf-8').read(),
+    long_description=long_description,
     author='Michal Kielan',
     author_email='michalkielan@protonmail.com',
     url='https://github.com/michalkielan/simple-adb',
